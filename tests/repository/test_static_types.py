abc76d6f48fefe95a7e8fe1b8ef467b2eae1f355
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-11-23 18:03:50 +0100

    Compose initial ivy template (3)
    Add static type tests

---
+++ tests/repository/test_static_types.py
@@ -0,0 +1,17 @@
+import os
+
+PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
+
+
+def _rm(path: str) -> None:
+    os.system(f"rm -rf {os.path.join(PROJECT_DIR, path)}")
+
+
+def test_static_types() -> None:
+    _rm(".mypy_cache/3.*/src")
+    _rm(".mypy_cache/3.*/tests")
+    _rm(".mypy_cache/3.*/run.*")
+
+    for path in ["src/", "tests/", "run.py"]:
+        assert os.system(
+            f"cd {PROJECT_DIR} && .venv/bin/python -m mypy {path}") == 0
