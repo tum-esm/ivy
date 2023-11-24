fa21c2b508a3ba53c0e892b2d025906c5401f4e0
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-11-24 02:03:26 +0100

    Reimplement the updater (1)
    Add config

--- src/constants.py
+++ src/constants.py
@@ -4,3 +4,4 @@ VERSION_REGEX = r"^\d+\.\d+\.\d+(-(alpha|beta|rc)\.\d+)?$"
 PROJECT_DIR = os.path.dirname(os.path.dirname(__file__))
 VERSION = "0.1.0"
 NAME = "ivy-seed"
+IVY_ROOT_DIR = os.path.join(os.path.expanduser('~'), "Documents", NAME)
--- src/types/config.py
+++ src/types/config.py
@@ -1,11 +1,27 @@
 from __future__ import annotations
 import os
+from typing import Literal, Optional
 import pydantic
 import src
 
 
+class UpdaterConfig(pydantic.BaseModel):
+    repository: str = pydantic.Field(
+        ..., pattern=r"[a-zA-Z0-9-]+/[a-zA-Z0-9-]+"
+    )
+    provider: Literal["github", "gitlab"] = pydantic.Field(...)
+    provider_host: str = pydantic.Field(..., min_length=3)
+    access_token: Optional[str] = pydantic.Field(
+        None, min_length=3, max_length=256
+    )
+    source_conflict_strategy: Literal["overwrite",
+                                      "reuse"] = pydantic.Field("reuse")
+
+
 class Config(pydantic.BaseModel):
+    model_config = pydantic.ConfigDict(extra="forbid")
     version: str = pydantic.Field(..., pattern=src.constants.VERSION_REGEX)
+    updater: Optional[UpdaterConfig] = pydantic.Field(None)
 
     @staticmethod
     def load() -> Config:
