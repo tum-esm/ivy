b9500511a0812838b5173a2943a260d24c015305
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-11-24 03:15:37 +0100

    Add test for repository connection

---
+++ tests/integration/test_repository_connection.py
@@ -0,0 +1,32 @@
+import pytest
+import src
+
+
+@pytest.mark.integration
+def test_repository_connection() -> None:
+    config = src.types.Config.load()
+    if config.updater is None:
+        return
+
+    if config.updater.provider == 'github':
+        headers = '--header "Accept: application/vnd.github+json" --header "X-GitHub-Api-Version: 2022-11-28"'
+        if config.updater.access_token is not None:
+            headers += f' --header "Authorization: Bearer {config.updater.access_token}"'
+        try:
+            src.utils.functions.run_shell_command(
+                f'curl --fail -L {headers} https://api.{config.updater.provider_host}/repos/{config.updater.repository}/readme'
+            )
+        except Exception as e:
+            raise Exception(f'Could not connect to repository: {e}')
+
+    if config.updater.provider == 'gitlab':
+        headers: str = ""
+        if config.updater.access_token is not None:
+            headers += f'--header "PRIVATE-TOKEN: {config.updater.access_token}"'
+        try:
+            src.utils.functions.run_shell_command(
+                f'curl -L --fail {headers} https://{config.updater.provider_host}/api/v4/projects/{config.updater.repository.replace("/", "%2F")}/repository/tags?search=v0.0.0'
+            )
+        except Exception as e:
+            raise Exception(f'Could not connect to repository: {e}')
+
