8883fda5552951e520543c8bcd938849e46bef8d
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-11-24 21:25:32 +0100

    Add modules (3)
    Use modules in mainloop

--- src/main.py
+++ src/main.py
@@ -1,7 +1,22 @@
 """Main loop of the automation"""
 
+import time
+import src
+
 
 def run() -> None:
     """Run the automation"""
 
-    pass
+    config = src.types.Config.load()
+
+    modules: list[src.modules.module_base_class.ModuleBaseClass] = [
+        src.modules.SystemCheckModule(config),
+        src.modules.DummyProcedureModule(config)
+    ]
+
+    # TODO: add graceful teardown
+
+    while True:
+        for module in modules:
+            module.run()
+        time.sleep(5)
