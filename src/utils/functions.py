17e91569681063266ac885454f73d58b040528ee
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-11-24 02:03:55 +0100

    Reimplement the updater (2)
    Add `run_shell_command` from `tum_esm_utils`

--- src/utils/functions.py
+++ src/utils/functions.py
@@ -1,15 +1,60 @@
+import os
 import re
+import subprocess
+from typing import Optional
 import src
 
+version_regex = re.compile(src.constants.VERSION_REGEX)
 
-def string_is_valid_version_name(version_name: str) -> bool:
-    """Check if the version name is valid. Valid version name examples `v1.2.3`,
+
+def string_is_valid_version(version_string: str) -> bool:
+    """Check if the version string is valid. Valid version name examples `v1.2.3`,
     `v4.5.6-alpha.78`, `v7.8.9-beta.10`, `v11.12.13-rc.14`.
     
     Args:
-        version_name: version name to check.
+        version_string: version string to check.
     
     Returns:
-        True if the version name is valid, False otherwise."""
+        True if the version string is valid, False otherwise."""
 
-    return re.match(src.constants.VERSION_REGEX, version_name) is not None
+    return version_regex.match(version_string) is not None
+
+
+class CommandLineException(Exception):
+    def __init__(self, value: str, details: Optional[str] = None) -> None:
+        self.value = value
+        self.details = details
+        Exception.__init__(self)
+
+    def __str__(self) -> str:
+        return repr(self.value)
+
+
+def run_shell_command(
+    command: str,
+    working_directory: Optional[str] = None,
+    executable: str = "/bin/bash",
+) -> str:
+    """runs a shell command and raises a `CommandLineException`
+    if the return code is not zero, returns the stdout. Uses
+    `/bin/bash` by default."""
+
+    p = subprocess.run(
+        command,
+        shell=True,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        cwd=working_directory,
+        env=os.environ.copy(),
+        executable=executable,
+    )
+    stdout = p.stdout.decode("utf-8", errors="replace").strip()
+    stderr = p.stderr.decode("utf-8", errors="replace").strip()
+
+    if p.returncode != 0:
+        raise CommandLineException(
+            f"command '{command}' failed with exit code {p.returncode}",
+            details=f"\nstderr:\n{stderr}\nstout:\n{stdout}",
+        )
+
+    return stdout
