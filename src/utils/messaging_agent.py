f6bdc88a4cf3ea41e26f6a9ce05c526b3cdb986f
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-11-25 17:02:27 +0100

    Add messaging (3)
    Add messaging agent function to add, fetch and remove messages

--- src/utils/messaging_agent.py
+++ src/utils/messaging_agent.py
@@ -26,3 +26,62 @@ class MessagingAgent():
                     );
                 """
             )
+
+    def add_message(
+        self,
+        message_body: Union[
+            src.types.DataMessageBody,
+            src.types.LogMessageBody,
+            src.types.ConfigMessageBody,
+        ],
+    ) -> None:
+        with self.connection:
+            self.connection.execute(
+                """
+                    INSERT INTO QUEUE (timestamp, message_body)
+                    VALUES (?);
+                """,
+                (int(time.time()), message_body.model_dump_json()),
+            )
+
+    def get_n_latest_messages(
+        self,
+        n: int,
+        excluded_message_ids: list[int] = []
+    ) -> list[src.types.MessageQueueItem]:
+        with self.connection:
+            results = list(
+                self.connection.execute(
+                    """
+                        SELECT internal_id, timestamp, message_body
+                        FROM QUEUE
+                        WHERE internal_id NOT IN (?)
+                        ORDER BY timestamp DESC
+                        LIMIT ?;
+                    """,
+                    (excluded_message_ids, n),
+                ).fetchall()
+            )
+        assert len(results) <= n
+        for result in results:
+            assert len(result) == 3
+            assert isinstance(result[0], int)
+            assert isinstance(result[1], int)
+            assert isinstance(result[2], str)
+        return [
+            src.types.MessageQueueItem(
+                internal_id=result[0],
+                timestamp=result[1],
+                message_body=result[2],
+            ) for result in results
+        ]
+
+    def remove_messages(self, message_ids: list[int]) -> None:
+        with self.connection:
+            self.connection.execute(
+                """
+                    DELETE FROM QUEUE
+                    WHERE internal_id IN (?);
+                """,
+                (message_ids, ),
+            )
