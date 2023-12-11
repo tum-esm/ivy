23373cc54107ac38d3ce39184cee621ff56dfdfc
Moritz Makowski <moritz@dostuffthatmatters.dev>
2023-12-12 00:30:51 +0100

    Fix weird JSON schema nesting

--- docs/components/config-schema-object.ts
+++ docs/components/config-schema-object.ts
@@ -1,148 +1,5 @@
 /* prettier-ignore */
 const CONFIG_SCHEMA_OBJECT: any = {
-    "$defs": {
-        "DummyProcedureConfig": {
-            "properties": {
-                "seconds_between_datapoints": {
-                    "description": "How many seconds should be between each datapoint in the dummy procedure",
-                    "maximum": 7200,
-                    "minimum": 1,
-                    "title": "Seconds Between Datapoints",
-                    "type": "integer"
-                }
-            },
-            "required": [
-                "seconds_between_datapoints"
-            ],
-            "title": "DummyProcedureConfig",
-            "type": "object"
-        },
-        "LoggingVerbosityConfig": {
-            "description": "How verbose to log to the different data streams.\n\nFor example, If the level is set to \"WARNING\", only warnings, errors\nand exceptions will be written to the respective data stream. If the\nlevel is set to \"DEBUG\", all logs will be written to the respective\ndata stream.\n\nImportance: DEBUG > INFO > WARNING > ERROR > EXCEPTION\n\nIf the level is set to None, no logs will be written to the respective\ndata stream.",
-            "properties": {
-                "file_archive": {
-                    "description": "The minimum log level for the file archive in `data/logs`",
-                    "enum": [
-                        "DEBUG",
-                        "INFO",
-                        "WARNING",
-                        "ERROR",
-                        "EXCEPTION",
-                        null
-                    ],
-                    "title": "File Archive"
-                },
-                "console_prints": {
-                    "description": "The minimum log level for the console prints",
-                    "enum": [
-                        "DEBUG",
-                        "INFO",
-                        "WARNING",
-                        "ERROR",
-                        "EXCEPTION",
-                        null
-                    ],
-                    "title": "Console Prints"
-                },
-                "message_sending": {
-                    "description": "The minimum log level for the message sending",
-                    "enum": [
-                        "DEBUG",
-                        "INFO",
-                        "WARNING",
-                        "ERROR",
-                        "EXCEPTION",
-                        null
-                    ],
-                    "title": "Message Sending"
-                }
-            },
-            "required": [
-                "file_archive",
-                "console_prints",
-                "message_sending"
-            ],
-            "title": "LoggingVerbosityConfig",
-            "type": "object"
-        },
-        "SystemChecksConfig": {
-            "properties": {
-                "seconds_between_checks": {
-                    "description": "How many seconds should be between each run of the system checks",
-                    "maximum": 7200,
-                    "minimum": 1,
-                    "title": "Seconds Between Checks",
-                    "type": "integer"
-                }
-            },
-            "required": [
-                "seconds_between_checks"
-            ],
-            "title": "SystemChecksConfig",
-            "type": "object"
-        },
-        "UpdaterConfig": {
-            "properties": {
-                "repository": {
-                    "description": "The repository in which this source code is hosted, i.e. 'orgname/reponame'",
-                    "pattern": "[a-zA-Z0-9-]+/[a-zA-Z0-9-]+",
-                    "title": "Repository",
-                    "type": "string"
-                },
-                "provider": {
-                    "description": "You can suggest more providers in the issue tracker",
-                    "enum": [
-                        "github",
-                        "gitlab"
-                    ],
-                    "title": "Provider",
-                    "type": "string"
-                },
-                "provider_host": {
-                    "examples": [
-                        "github.com",
-                        "gitlab.com",
-                        "gitlab.yourcompany.com"
-                    ],
-                    "minLength": 3,
-                    "title": "Provider Host",
-                    "type": "string"
-                },
-                "access_token": {
-                    "anyOf": [
-                        {
-                            "maxLength": 256,
-                            "minLength": 3,
-                            "type": "string"
-                        },
-                        {
-                            "type": "null"
-                        }
-                    ],
-                    "default": null,
-                    "description": "The access token to use for the provider. If the repository is public, this can be left empty.",
-                    "title": "Access Token"
-                },
-                "source_conflict_strategy": {
-                    "default": "reuse",
-                    "description": "The strategy to follow, when upgrading to a new version and the source code already exists. 'reuse' will keep the existing source code but create a new venv. 'overwrite' will remove the existing code directory and create a new one. You can keep this at 'reuse' by default and if a version upgrade fails during the download phase, temporarily change this to 'overwrite' to force a redownload of the source code.",
-                    "enum": [
-                        "overwrite",
-                        "reuse"
-                    ],
-                    "title": "Source Conflict Strategy",
-                    "type": "string"
-                }
-            },
-            "required": [
-                "repository",
-                "provider",
-                "provider_host"
-            ],
-            "title": "UpdaterConfig",
-            "type": "object"
-        }
-    },
     "additionalProperties": false,
     "description": "Schema of the config file for this version of the software.\n\nA rendered API reference can be found in the documentation at TODO.",
     "properties": {
@@ -278,46 +135,38 @@ const CONFIG_SCHEMA_OBJECT: any = {
             "description": "If this is not set, the updater will not be used."
         },
         "dummy_procedure": {
-            "allOf": [
-                {
-                    "properties": {
-                        "seconds_between_datapoints": {
-                            "description": "How many seconds should be between each datapoint in the dummy procedure",
-                            "maximum": 7200,
-                            "minimum": 1,
-                            "title": "Seconds Between Datapoints",
-                            "type": "integer"
-                        }
-                    },
-                    "required": [
-                        "seconds_between_datapoints"
-                    ],
-                    "title": "DummyProcedureConfig",
-                    "type": "object"
+            "description": "Settings for the dummy procedure.",
+            "properties": {
+                "seconds_between_datapoints": {
+                    "description": "How many seconds should be between each datapoint in the dummy procedure",
+                    "maximum": 7200,
+                    "minimum": 1,
+                    "title": "Seconds Between Datapoints",
+                    "type": "integer"
                 }
+            },
+            "required": [
+                "seconds_between_datapoints"
             ],
-            "description": "Settings for the dummy procedure."
+            "title": "DummyProcedureConfig",
+            "type": "object"
         },
         "system_checks": {
-            "allOf": [
-                {
-                    "properties": {
-                        "seconds_between_checks": {
-                            "description": "How many seconds should be between each run of the system checks",
-                            "maximum": 7200,
-                            "minimum": 1,
-                            "title": "Seconds Between Checks",
-                            "type": "integer"
-                        }
-                    },
-                    "required": [
-                        "seconds_between_checks"
-                    ],
-                    "title": "SystemChecksConfig",
-                    "type": "object"
+            "description": "Settings for the system checks procedure.",
+            "properties": {
+                "seconds_between_checks": {
+                    "description": "How many seconds should be between each run of the system checks",
+                    "maximum": 7200,
+                    "minimum": 1,
+                    "title": "Seconds Between Checks",
+                    "type": "integer"
                 }
+            },
+            "required": [
+                "seconds_between_checks"
             ],
-            "description": "Settings for the system checks procedure."
+            "title": "SystemChecksConfig",
+            "type": "object"
         }
     },
     "required": [
--- docs/scripts/export_config_schema.py
+++ docs/scripts/export_config_schema.py
@@ -1,8 +1,10 @@
+from typing import Any
 import json
 import os
 import sys
 import jsonref
 import re
+import pydantic
 
 PROJECT_DIR = os.path.dirname(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -17,17 +19,53 @@ sys.path.append(PROJECT_DIR)
 
 import src
 
+
+def _remove_allof_wrapping(o: dict[str, Any]) -> dict[str, Any]:
+    if "properties" in o.keys():
+        return {
+            **o,
+            "properties": {
+                k: _remove_allof_wrapping(v)
+                for k, v in o["properties"].items()
+            },
+        }
+    elif "allOf" in o.keys():
+        assert len(o["allOf"]) == 1
+        return {
+            **{k: v
+               for k, v in o.items() if k != "allOf"},
+            **o["allOf"][0],
+        }
+    else:
+        return o
+
+
+def generate_readable_schema(o: pydantic.BaseModel) -> str:
+    # remove $ref usages
+    schema_without_refs = jsonref.loads(
+        json.dumps(o.model_json_schema(by_alias=False))
+    )
+
+    # remove $defs section
+    schema_without_defs = json.loads(
+        jsonref.dumps(schema_without_refs, indent=4)
+    )
+    del schema_without_defs["$defs"]
+
+    # convert weird "allOf" wrapping to normal wrapping
+    schema_without_allofs = _remove_allof_wrapping(schema_without_defs)
+
+    return json.dumps(schema_without_allofs, indent=4)
+
+
 # ---------------------------------------------------------
 # Update the config schema object in the documentation
 
 print(f"Exporting config schema object to {CONFIG_JSON_TARGET}")
-dereferenced_config_schema = jsonref.loads(
-    json.dumps(src.types.Config.model_json_schema(by_alias=False))
-)
 with open(CONFIG_JSON_TARGET, "w") as f:
     f.write(
         "/* prettier-ignore */\n" + "const CONFIG_SCHEMA_OBJECT: any = " +
-        json.dumps(dereferenced_config_schema, indent=4) +
+        generate_readable_schema(src.types.Config) +
         ";\n\nexport default CONFIG_SCHEMA_OBJECT;"
     )
 
