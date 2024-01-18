d4370b12ece8f9151efc54be653e23e655933b58
Moritz Makowski <moritz@dostuffthatmatters.dev>
2024-01-18 16:16:37 +0100

    Refactor config (3)
    Extend tests for config

--- tests/repository/test_config.py
+++ tests/repository/test_config.py
@@ -1,19 +1,32 @@
+import pydantic
 import pytest
 import src
 
 
 @pytest.mark.ci
-def test_config_template() -> None:
-    """Test whether the config template is parseable"""
+def test_config_templates() -> None:
+    """Test whether the config template is parseable using the
+    config schema and the foreign config schema."""
+
     config = src.types.Config.load_template()
     assert config.version == src.constants.VERSION, "Version in config.template.json is not the same as in src/constants.py"
 
-
-@pytest.mark.ci
-def test_foreign_config() -> None:
-    """Test wether the config template which is a valid config can be parsed
-    by the foreign config schema. I.e. all valid configs should also be valid
-    foreign configs."""
     src.types.ForeignConfig.model_validate_json(
         src.types.Config.load_template().model_dump_json()
     )
+
+
+@pytest.mark.ci
+def test_config_validation() -> None:
+    """Test whether the config validation fails as expected."""
+    try:
+        src.types.Config.model_validate({})
+        raise AssertionError("Validation should have failed.")
+    except pydantic.ValidationError:
+        pass
+
+    try:
+        src.types.ForeignConfig.model_validate({})
+        raise AssertionError("Validation should have failed.")
+    except pydantic.ValidationError:
+        pass
