bc16d48ed20c88cb1118a1b4f31025fe14807800
Moritz Makowski <moritz@dostuffthatmatters.dev>
2024-02-03 01:16:28 +0100

    Write docs (1)
    Use nextra settings from EM27 Retrieval Pipeline

--- docs/postcss.config.js
+++ docs/postcss.config.js
@@ -3,8 +3,6 @@
 /** @type {import('postcss').Postcss} */
 module.exports = {
   plugins: {
-    "postcss-import": {},
-    "tailwindcss/nesting": "postcss-nesting",
     tailwindcss: {},
     autoprefixer: {},
   },
