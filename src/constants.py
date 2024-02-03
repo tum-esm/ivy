3e1bf3860a90acb901800f5d5c4dc2bce6584bca
Moritz Makowski <moritz@dostuffthatmatters.dev>
2024-02-03 05:20:12 +0100

    Write docs (3)
    Update comments, make some classes private

--- src/constants.py
+++ src/constants.py
@@ -1,28 +1,37 @@
 import os
-from typing import Literal
+from typing import Literal, Annotated
 
-VERSION_REGEX: str = r"^\d+\.\d+\.\d+(-(alpha|beta|rc)\.\d+)?$"
-"""Valid version name examples `1.2.3`, `4.5.6-alpha.78`, `7.8.9-beta.10`, `11.12.13-rc.14`"""
+VERSION_REGEX: Annotated[
+    str,
+    "Valid version name examples `1.2.3`, `4.5.6-alpha.78`, `7.8.9-beta.10`, `11.12.13-rc.14`"
+] = r"^\d+\.\d+\.\d+(-(alpha|beta|rc)\.\d+)?$"
 
-PROJECT_DIR = os.path.dirname(os.path.dirname(__file__))
-"""The root directory of the project (the parent of `src/`)"""
+PROJECT_DIR: Annotated[
+    str,
+    "The root directory of the project (the parent of `src/`)",
+] = os.path.dirname(os.path.dirname(__file__))
 
-VERSION = "0.1.0"
-"""The current version of the project"""
+VERSION: Annotated[
+    str,
+    "The current version of the project",
+] = "0.1.0"
 
-NAME = "ivy-seed"
-"""The name of the project"""
+NAME: Annotated[str, "The name of the project"] = "ivy-seed"
 
-IVY_ROOT_DIR = os.path.join(os.path.expanduser('~'), "Documents", NAME)
-"""The root directory of the project on a production system = `~/Documents/{NAME}`"""
+IVY_ROOT_DIR: Annotated[
+    str,
+    "The root directory of the project on a production system = `~/Documents/{NAME}`",
+] = os.path.join(os.path.expanduser('~'), "Documents", NAME)
 
-LOGGING_LEVEL_PRIORITIES: dict[
-    Literal["DEBUG", "INFO", "WARNING", "ERROR", "EXCEPTION"],
-    int,
+LOGGING_LEVEL_PRIORITIES: Annotated[
+    dict[
+        Literal["DEBUG", "INFO", "WARNING", "ERROR", "EXCEPTION"],
+        int,
+    ],
+    "Order of the logging levels from the lowest to the highest, high number means high priority",
 ] = {"DEBUG": 0, "INFO": 1, "WARNING": 2, "ERROR": 3, "EXCEPTION": 4}
-"""Order of the logging levels from the lowest to
-the highest, high number means high priority"""
 
-SECONDS_PER_GRACEFUL_PROCEDURE_TEARDOWN: int = 10
-"""Number of seconds to wait for a procedure process
-to tear down gracefully before killing it"""
+SECONDS_PER_GRACEFUL_PROCEDURE_TEARDOWN: Annotated[
+    int,
+    "Number of seconds to wait for a procedure process to tear down gracefully before killing it"
+] = 10
