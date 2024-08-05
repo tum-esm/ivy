de1b58c7e47a4825b5a99bc86d897525cc23701f
Moritz Makowski <moritz@dostuffthatmatters.dev>
2024-08-05 21:55:57 +0200

    Add more comments and TODOs

---
+++ tests/repository/test_remote_configuration.py
@@ -0,0 +1,13 @@
+
+
+# TODO: Rename the two cases to "reconfiguration" and "version_change"
+# TODO: run this tests for a github repo
+# TODO: run this tests for a gitlab repo
+
+def test_reconfiguration() -> None:
+    # TODO
+    pass
+
+def test_version_update() -> None:
+    # TODO
+    pass
