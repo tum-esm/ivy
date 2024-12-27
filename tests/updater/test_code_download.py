34de42dc675842ea2e7bcfa5d11184bbba990115
Moritz Makowski <moritz@dostuffthatmatters.dev>
2024-12-27 17:32:13 +0100

    Add updateability test (10)
    finish downloader tests
    debug downloader

--- tests/updater/test_code_download.py
+++ tests/updater/test_code_download.py
@@ -1,4 +1,7 @@
+import os
+import shutil
 import pytest
+import tum_esm_utils
 import src
 
 
@@ -12,7 +15,21 @@ def test_code_github_download() -> None:
         access_token=None,
         source_conflict_strategy="reuse",
     )
-    # 2.1.0
+    version = tum_esm_utils.validators.Version("2.1.0")
+    target_dir = os.path.join(src.constants.IVY_ROOT_DIR, version.as_identifier())
+    assert os.path.isdir(
+        src.constants.IVY_ROOT_DIR
+    ), f"IVY_ROOT_DIR ({src.constants.IVY_ROOT_DIR}) does not exist"
+    assert not os.path.exists(target_dir), f"Target directory ({target_dir}) already exists"
+    if "1.3.2" in os.listdir(src.constants.IVY_ROOT_DIR):
+        shutil.rmtree(os.path.join(src.constants.IVY_ROOT_DIR, "1.3.2"))
+
+    # download source code
+    src.utils.Updater.download_source_code(updater_config, version)
+
+    assert os.path.isdir(target_dir), f"Target directory ({target_dir}) does not exist"
+    files = os.listdir(src.constants.IVY_ROOT_DIR)
+    assert len(files) == 1, f"Unexpected files: {files}"
 
 
 @pytest.mark.order(4)
@@ -25,4 +42,19 @@ def test_code_gitlab_download() -> None:
         access_token=None,
         source_conflict_strategy="reuse",
     )
-    # v1.3.2
+    version = tum_esm_utils.validators.Version("1.3.2")
+    target_dir = os.path.join(src.constants.IVY_ROOT_DIR, version.as_identifier())
+    assert os.path.isdir(
+        src.constants.IVY_ROOT_DIR
+    ), f"IVY_ROOT_DIR ({src.constants.IVY_ROOT_DIR}) does not exist"
+    if "2.1.0" in os.listdir(src.constants.IVY_ROOT_DIR):
+        shutil.rmtree(os.path.join(src.constants.IVY_ROOT_DIR, "2.1.0"))
+
+    assert not os.path.exists(target_dir), f"Target directory ({target_dir}) already exists"
+
+    # download source code
+    src.utils.Updater.download_source_code(updater_config, version)
+
+    assert os.path.isdir(target_dir), f"Target directory ({target_dir}) does not exist"
+    files = os.listdir(src.constants.IVY_ROOT_DIR)
+    assert len(files) == 1, f"Unexpected files: {files}"
