7d54eae151a92ccf21d725410f8281a67ce26cb0
Moritz Makowski <moritz@dostuffthatmatters.dev>
2024-12-27 16:54:44 +0100

    Add updateability test (6)
    add empty updater functions

---
+++ tests/updater/test_code_download.py
@@ -0,0 +1,26 @@
+import pytest
+import src
+
+
+@pytest.mark.updater
+def test_code_github_download() -> None:
+    updater_config = src.types.UpdaterConfig(
+        repository="tum-esm/utils",
+        provider="github",
+        provider_host="github.com",
+        access_token=None,
+        source_conflict_strategy="reuse",
+    )
+    # 2.1.0
+
+
+@pytest.mark.updater
+def test_code_gitlab_download() -> None:
+    updater_config = src.types.UpdaterConfig(
+        repository="coccon-kit/proffastpylot",
+        provider="gitlab",
+        provider_host="gitlab.eudat.eu",
+        access_token=None,
+        source_conflict_strategy="reuse",
+    )
+    # v1.3.2
